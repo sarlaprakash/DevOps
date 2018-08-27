3db6cc8eab80b1d352a67e1057e5e0da9d04edea
prakash <sarlaprakash>
2018-08-26 22:35:00 -0700

    initial commit

---
+++ calc.py
@@ -0,0 +1,20 @@
+def add(x,y):
+	return x+y
+
+def substract(x,y):
+	return x-y
+
+def multiply(x,y):
+	return(x*y)
+
+def divide(x,y):
+	return(x/y)
+
+x=input()
+y=input()
+
+print add(x,y)
+print multiply(x,y)
+print substract(x,y)
+print divide(x,y)
+
